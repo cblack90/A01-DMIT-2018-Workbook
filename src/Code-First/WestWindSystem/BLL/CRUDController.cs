86b9325062072f2ee857afd0e25d4af7bd5864bf
Chris Black <chrisblack90@gmail.com>
2019-09-13 13:34:03 -0600

    Added CRUD methods for suppliers, categories, addresses

---
+++ src/Code-First/WestWindSystem/BLL/CRUDController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    [DataObject]// for the <ObjectDataSource> control
+    public class CRUDController
+    {
+        #region Products CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Product> ListProducts()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        #endregion
+        //methods for suppliers, categories, addresses
+        #region Suppliers CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Supplier> ListSuppliers()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Suppliers.ToList();
+            }
+        }
+        #endregion
+
+        #region Categories CRUD
+        public List<Category> ListCategories()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        #endregion
+
+        #region Addresses CRUD
+        public List<Address> ListAddresses()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Addresses.ToList();
+            }
+        }
+        #endregion
+    }
+}
