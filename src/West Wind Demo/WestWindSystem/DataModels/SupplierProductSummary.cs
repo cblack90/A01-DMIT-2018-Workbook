89a55a48ff90fb7fe811a6f8781ba6b7aecff044
Chris Black <chrisblack90@gmail.com>
2019-09-26 11:34:41 -0600

    Re #2 Built Read Model classes

---
+++ src/West Wind Demo/WestWindSystem/DataModels/SupplierProductSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DataModels
+{
+    public class SupplierProductSummary
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public double CategoryName { get; set; }
+    }
+}
