540ff45a1c4352ba9b2b66028951aa23f028ba25
Chris Black <chrisblack90@gmail.com>
2019-11-07 10:34:12 -0700

    Demo'd why to make context and entities as internal classes

--- src/Authentication Demo/Custom Identity/DemoSystem2/Entities/DemoContext.cs
+++ src/Authentication Demo/Custom Identity/DemoSystem2/Entities/DemoContext.cs
@@ -5,7 +5,7 @@ namespace DemoSystem2.Entities
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
 
-    public partial class DemoContext : DbContext
+    internal partial class DemoContext : DbContext
     {
         public DemoContext()
             : base("name=DefaultConnection")
